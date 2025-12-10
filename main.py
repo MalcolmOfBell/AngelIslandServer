1e0e53e6c5137b2d9eb813fd50482a3ff59f4d98
Mal <malcolmofbell@gmail.com>
2025-12-10 21:32:34 +0000

    Fixed(?) ssl context bug

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ app = Flask(__name__)
 # }
 app.config['MONGODB_SETTINGS'] = {
     'db': 'angelisland_db',
-    'host': "localhost",
+    'host': "192.168.1.174",
     'port': 27017,
 }
 app.secret_key = 'some_key'
